f47312bcdc7ee7701dee3dfca666f10e1c127dcc
roma_hurnovich <zebomenn@bk.ru>
2024-10-14 21:01:23 +0300

    merge header

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     react(),
     createHtmlPlugin({
       minify: true,
-      entry: "main.tsx",
+      entry: "src/main.tsx",
       template: "src/index.html",
     }),
   ],
